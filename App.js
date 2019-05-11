f1a8c4365ee82409da9d15ac45f1a812824fcdb6
Ryan Silva <RyandotSilva@gmail.com>
2019-05-11 10:39:25 -0400

    Add jsx to render main App component

--- App.js
+++ App.js
@@ -11,8 +11,22 @@ const Todo = props => (
 export default class App extends React.Component {
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
+			<View>
+				<Text>Todo count: {this.state.todos.length}</Text>
+				<Text>
+					Unchecked todo count:{' '}
+					{this.state.todos.filter(todo => !todo.checked).length}
+				</Text>
+				<Button onPress={() => this.addTodo()} tlte="Add TODO" />
+				<ScrollView>
+					{this.state.todos.map(todo => (
+						<Todo
+							onToggle={() => this.toggleTodo(todo.id)}
+							onDelete={() => this.removeTodo(todo.id)}
+							todo={todo}
+						/>
+					))}
+				</ScrollView>
 			</View>
 		);
 	}
